4c0e18ff90d9cae782298c8ebfc38ac12874954e
Saryn Mooney <smooney15@my.whitworth.edu>
2020-12-24 16:48:38 -0700

    query for user posts

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -15,15 +15,5 @@ module Types
     def get_user(id:)
       User.find(id)
     end
-
-
-
-
-    # TODO: remove me
-    # field :test_field, String, null: false,
-    #   description: "An example field added by the generator"
-    # def test_field
-    #   "Hello World!"
-    # end
   end
 end
