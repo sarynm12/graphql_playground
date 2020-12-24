069e3ff9cbd426542bb0ebfbd8f5750140e7cfb6
Saryn Mooney <smooney15@my.whitworth.edu>
2020-12-23 20:36:23 -0700

    create queries for user

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,12 +2,28 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    field :get_users, [Types::UserType], null: false, description: 'This will return all users'
+    field :get_user, Types::UserType, null: false do
+      description 'This will return a single user'
+      argument :id, ID, required: true
+    end
+
+    def get_users
+      User.all
+    end
+
+    def get_user(id:)
+      User.find(id)
+    end
+
+
+
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World!"
+    # end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates_presence_of :name, :email
   has_many :posts
 end
+
+#https://mattboldt.com/2019/01/07/rails-and-graphql/
